2d4b3ebf16da54022883bc5ded82f411d2d41c0f
coolas7 <sabakas7@gmail.com>
2024-04-22 22:41:33 +0300

    ppp

--- src/Utils/FilesCache.php
+++ src/Utils/FilesCache.php
@@ -2,70 +2,26 @@
 
 namespace App\Utils;
 
-use App\Utils\Interfaces\CacheInterface;
 
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
-
-use Doctrine\Common\Cache\CacheProvider;
-use Doctrine\Common\Cache\SQLite3Cache;
-use Symfony\Component\Cache\Adapter\DoctrineAdapter;
+use App\Utils\Interfaces\CacheInterface;
 
 
-class FilesCache implements CacheInterface
+
+class FilesCache implements CacheInterface 
 {
 
-    public $cache;
-    public function __construct()
-    {
-	// not working on free Heroku account:
-        // $this->cache =  new TagAwareAdapter(
-        // new FilesystemAdapter()
-        // );  
+	public $cache;
 
-        $provider = new SQLite3Cache(new \SQLite3(__DIR__ . '/cache/data.db'), 'TableName');
 
-        $this->cache =  new TagAwareAdapter(
+	public function __construct()
+	{
 
-            new DoctrineAdapter(
+		$this->cache = new TagAwareAdapter(
+			new FilesystemAdapter()
+		);
 
-                // a cache provider instance
-                $provider,
+	}
 
-                // a string prefixed to the keys of the items stored in this cache
-                $namespace = '',
-
-                // the default lifetime (in seconds) for cache items that do not define their
-                // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
-                // until the database table is truncated or its rows are otherwise deleted)
-                $defaultLifetime = 0
-            )
-        );
-    }
 }
-
-// namespace App\Utils;
-
-
-// use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-// use Symfony\Component\Cache\Adapter\TagAwareAdapter;
-// use App\Utils\Interfaces\CacheInterface;
-
-
-
-// class FilesCache implements CacheInterface 
-// {
-
-// 	public $cache;
-
-
-// 	public function __construct()
-// 	{
-
-// 		$this->cache = new TagAwareAdapter(
-// 			new FilesystemAdapter()
-// 		);
-
-// 	}
-
-// }
